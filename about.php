473c06df744ba6a2dc4a55e224d5a8564980c3f6
EJ ANTON S POTOT <zuming1404@gmail.com>
2020-01-07 01:42:28 +0800

    updated about

--- about.php
+++ about.php
@@ -89,17 +89,15 @@
 
   <!--Main layout-->
   <main>
-    <div class="container-fluid my-5 ">
-
-
+    <div class="container my-5 py-5">
       <!--Section: Content-->
       <section class="dark-grey-text">
     
-        <div class="row pr-lg-5">
+        <div class="row pr-lg-5 ">
           <div class="col-md-7 mb-4">
     
-            <div class="view">
-              <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="smaple image">
+            <div class="view ">
+              <img src="images/logo/logo.jpg" class="img-fluid" alt="smaple image">
             </div>
     
           </div>
@@ -115,12 +113,65 @@
             </div>
           </div>
         </div>
-    
       </section>
       <!--Section: Content-->
-    
-    
+     
     </div>
+<div class="container ">
+  <hr>
+</div>
+  
+    <div class="container mb-5 px-5 pt-5 pb-3">
+
+  
+  <!--Section: Content-->
+  <section class="text-center dark-grey-text">
+    
+    <h6 class="font-weight-normal text-uppercase font-small grey-text mb-4">Features</h6>
+    <!-- Section heading -->
+    <h2 class="font-weight-bold">More Than You Think</h2>
+    <hr class="w-header my-4">
+    <!-- Section description -->
+    <p class="text-muted mx-auto mb-5">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit id laborum.</p>
+
+    <div class="row text-center d-flex justify-content-center">
+      <div class="col-lg-4 col-md-12 mb-4">
+        <div class="view card-img-100 mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/illustrations/drawkit-drawing-man-colour.svg" class="img-fluid" alt="smaple image">
+        </div>
+        <h5 class="font-weight-normal mb-4">Be the first</h5>
+        <p class="text-muted px-2 mb-lg-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="view card-img-100 mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/illustrations/drawkit-phone-conversation-colour.svg" class="img-fluid" alt="smaple image">
+        </div>
+        <h5 class="font-weight-normal mb-4">Skyrocket You Sells</h5>
+        <p class="text-muted px-2 mb-lg-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="view card-img-100 mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/illustrations/app-user-colour.svg" class="img-fluid" alt="smaple image">
+        </div>
+        <h5 class="font-weight-normal mb-4">Acquire Potential Users</h5>
+        <p class="text-muted px-2 mb-md-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+    
+  </section>
+  <!--Section: Content-->
+
+
+</div>      
   </main>
   <!--Main layout-->
   <!-- footer -->
